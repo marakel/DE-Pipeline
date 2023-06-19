7f2cbdd727e1c761e5fc7cbb13537b1b9640a8c0
marakel <52334255+marakel@users.noreply.github.com>
2023-06-19 13:03:39 -0400

    Update ingest_data.py

--- ingest_data.py
+++ ingest_data.py
@@ -13,7 +13,7 @@ def main(params):
     table_name = params.table_name
     url = params.url
     
-    # the backup files are gzipped, and it's important to keep the correct extension
+    # The files are gzipped, need to keep the correct extension
     # Opening the files in pandas
     if url.endswith('.csv.gz'):
         csv_name = 'output.csv.gz'
@@ -21,35 +21,26 @@ def main(params):
         csv_name = 'output.csv'
 
     os.system(f"wget {url} -O {csv_name}")
-
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
-
     df = next(df_iter)
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
-
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
 
-    while True: 
-
+    while True:
         try:
             t_start = time()
             
             df = next(df_iter)
-
             df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
             df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
             df.to_sql(name=table_name, con=engine, if_exists='append')
 
             t_end = time()
-
             print('Inserted another chunk, took %.3f second' % (t_end - t_start))
 
         except StopIteration:
@@ -58,7 +49,6 @@ def main(params):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
-
     parser.add_argument('--user', required=True, help='user name for postgres')
     parser.add_argument('--password', required=True, help='password for postgres')
     parser.add_argument('--host', required=True, help='host for postgres')
@@ -68,5 +58,4 @@ if __name__ == '__main__':
     parser.add_argument('--url', required=True, help='url of the csv file')
 
     args = parser.parse_args()
-
     main(args)
